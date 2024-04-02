11ad3b5e68dcda3df27d520a24e1c469eb0f262d
Goldman Kim <bbalganjjm@gmail.com>
2024-04-02 17:05:03 +0900

    Update.

--- src/main/java/common/app/sample/SampleController.java
+++ src/main/java/common/app/sample/SampleController.java
@@ -1,15 +1,13 @@
 package common.app.sample;
 
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
+import common.app.sample.service.impl.SampleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import common.app.sample.service.impl.SampleServiceImpl;
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author KIM HWANG MAN( bbalganjjm@gmail.com )
@@ -38,9 +36,9 @@ public class SampleController {
         return sampleService.getSample(vo);
     }
 
-    @RequestMapping("saveSample.json")
-    public Map<String, Object> saveSample(@RequestBody List<Map<String, Object>> voList) {
-        return sampleService.saveSample(voList);
+    @RequestMapping("saveSampleList.json")
+    public Map<String, Object> saveSampleList(@RequestBody List<Map<String, Object>> voList) {
+        return sampleService.saveSampleList(voList);
     }
 
     @RequestMapping("insertSample.json")
--- src/main/java/common/app/sample/service/impl/SampleServiceImpl.java
+++ src/main/java/common/app/sample/service/impl/SampleServiceImpl.java
@@ -1,14 +1,13 @@
 package common.app.sample.service.impl;
 
+import common.app.sample.mappers.SampleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import common.app.sample.mappers.SampleMapper;
 
 /**
  * @author KIM HWANG MAN( bbalganjjm@gmail.com )
@@ -33,7 +32,7 @@ public class SampleServiceImpl {
 		return sampleMapper.getSample(vo);
 	}
 
-	public Map<String, Object> saveSample(List<Map<String, Object>> voList) {
+	public Map<String, Object> saveSampleList(List<Map<String, Object>> voList) {
 		Iterator<Map<String, Object>> iter = voList.iterator();
 
 		Map<String, Object> vo;
