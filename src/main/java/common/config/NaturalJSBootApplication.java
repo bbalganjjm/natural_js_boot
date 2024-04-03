d65dc83b5b7a9c4564c4c19f82fe7db3a24355c3
Goldman Kim <bbalganjjm@gmail.com>
2024-04-03 15:48:43 +0900

    Update.

--- src/main/java/common/config/NaturalJSBootApplication.java
+++ src/main/java/common/config/NaturalJSBootApplication.java
@@ -25,7 +25,7 @@ import javax.sql.DataSource;
 @EnableAspectJAutoProxy
 @EnableScheduling
 @EnableTransactionManagement
-@MapperScan(basePackages = { "common" })
+@MapperScan(basePackages = { "common", "naturaljs.natural_template_designer_server.file" })
 @ComponentScan(basePackages = { "common" }, excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Controller.class) })
 @PropertySource(value = { "classpath:config/common/data.properties",
