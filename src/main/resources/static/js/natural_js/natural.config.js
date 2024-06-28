f5e007437aea3ae3abc83e79842b8fb9501ec1ce
Goldman Kim <bbalganjjm@gmail.com>
2024-06-28 09:55:33 +0900

    Bug fixes.

--- src/main/resources/static/js/natural_js/natural.config.js
+++ src/main/resources/static/js/natural_js/natural.config.js
@@ -625,8 +625,8 @@
              * Sort sort indicator when sort function is activated, You can also enter HTML tags
              */
             "sortableItem" : {
-                "asc" : "▼",
-                "desc" : "▲"
+                "asc" : "▲",
+                "desc" : "▼"
             },
             /**
              * Multilingual messages
